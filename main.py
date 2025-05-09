8d362997d96a3059b7a57fdcf7eb600df49389a9
roger-macgyver <rives.rodge@gmail.com>
2025-05-09 15:07:53 -0500

    addeding sorting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_word_count
 from stats import get_character_count
+#from stats import sort_data
 def get_book_text(path_to_file):
     with open (path_to_file) as f:
         contents=f.read()
--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
+#counts the total number of words in the book
 def get_word_count(contents):
     words=contents.split()
     count=len(words)
     count_msg= (f"{count} words found in the document")
     return count_msg
+#counts the total number of characters
 def get_character_count(contents):
     counts={}
     text=contents.lower()
@@ -12,3 +14,6 @@ def get_character_count(contents):
         else:
             counts[char] = 1
     return counts 
+#sorts data 
+#def sort_data(counts):
+    #return counts["num"]
