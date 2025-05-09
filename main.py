688d15aae40660504e48ee1c9241a34f6faa6f90
roger-macgyver <rives.rodge@gmail.com>
2025-05-09 14:11:20 -0500

    counter added

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ def get_book_text(path_to_file):
     with open (path_to_file) as f:
         contents=f.read()
     return contents
+def get_word_count(contents):
+    words=contents.split()
+    count=len(words)
+    count_msg= (f"{count} words found in the document")
+    return count_msg
+
 
 
 def main():
     contents = get_book_text("books/frankenstein.txt")
-    print contents   
-main 
+    count_msg = get_word_count(contents)
+    print (count_msg)  
+main()
